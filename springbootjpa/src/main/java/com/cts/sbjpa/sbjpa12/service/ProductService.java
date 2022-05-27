0723c827c43ff41dd4594797b43bdc030b019116
ritwicksamanta <ritwick.samanta001@gmail.com>
2022-05-27 15:33:05 +0530

    added java docs

--- springbootjpa/src/main/java/com/cts/sbjpa/sbjpa12/service/ProductService.java
+++ springbootjpa/src/main/java/com/cts/sbjpa/sbjpa12/service/ProductService.java
@@ -8,32 +8,64 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * The type Product service.
+ */
 @Service
 public class ProductService {
 
     @Autowired
     private ProductRepository pRepository;
 
+    /**
+     * Save product product.
+     *
+     * @param product the product
+     * @return the product
+     */
     public Product saveProduct(Product product){
 
         return pRepository.save(product);
     }
 
+    /**
+     * Get product by id product.
+     *
+     * @param id the id
+     * @return the product
+     */
     public Product getProductById(int id){
         return pRepository.findById(id).orElse(null);
 
     }
 
+    /**
+     * Delete product by id string.
+     *
+     * @param id the id
+     * @return the string
+     */
     public String deleteProductById(int id){
         pRepository.deleteById(id);
         return "product deleted,{},id";
 
     }
 
+    /**
+     * Get products list.
+     *
+     * @return the list
+     */
     public List<Product> getProducts(){
         return pRepository.findAll();
     }
 
+    /**
+     * Update product product.
+     *
+     * @param product the product
+     * @return the product
+     */
     public Product updateProduct(Product product){
         Product updateProduct=pRepository.getById(product.getId());
         updateProduct.setName(product.getName());
@@ -42,11 +74,24 @@ public class ProductService {
         return pRepository.save(updateProduct);
     }
 
+    /**
+     * Get products by name list.
+     *
+     * @param name the name
+     * @return the list
+     */
     public List<Product> getProductsByName(String name){
         return pRepository.findByName(name);
     }
 
 
+    /**
+     * Get products between quantities list.
+     *
+     * @param start the start
+     * @param end   the end
+     * @return the list
+     */
     public List<Product> getProductsBetweenQuantities(int start, int end){
         return pRepository.findByQuantityBetween(start,end);
     }
